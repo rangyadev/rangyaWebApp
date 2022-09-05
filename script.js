b031d57395afff7329bdb28928eef103f3aa5af7
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2022-09-05 15:13:29 +0800

    Backup Files

--- script.js
+++ script.js
@@ -8,19 +8,19 @@ let slideIndicatorsContainer = document.querySelector(".slideIndicators");
 //Background Components
 let mainPagePictureIndex = 0;
 let mainPicBackgroundCollection = [
-	"assets/homePageBackground/shoeAnnouncement_1.jpg",
-	"assets/homePageBackground/shoeAnnouncement_2.jpg",
-	"assets/homePageBackground/shoeAnnouncement_3.jpg",
-	"assets/homePageBackground/shoeAnnouncement_4.jpg",
-	"assets/homePageBackground/shoeAnnouncement_5.jpg",
+  "assets/homePageBackground/shoeAnnouncement_1.jpg",
+  "assets/homePageBackground/shoeAnnouncement_2.jpg",
+  "assets/homePageBackground/shoeAnnouncement_3.jpg",
+  "assets/homePageBackground/shoeAnnouncement_4.jpg",
+  "assets/homePageBackground/shoeAnnouncement_5.jpg",
 ];
 
 //Create Slide Indicators
 for (let i = 0; i < mainPicBackgroundCollection.length; i++) {
-	let slideIndicator = document.createElement("div");
-	slideIndicator.classList.add("slideIndicator");
-	slideIndicatorsContainer.appendChild(slideIndicator);
-	slideIndicators.push(slideIndicator);
+  let slideIndicator = document.createElement("div");
+  slideIndicator.classList.add("slideIndicator");
+  slideIndicatorsContainer.appendChild(slideIndicator);
+  slideIndicators.push(slideIndicator);
 }
 
 //Set Background on Load
@@ -30,42 +30,42 @@ slideIndicators[mainPagePictureIndex].style.backgroundColor = "white";
 //Changes Picture When Arrow is Clicked
 //Previous Picture
 changePicBack.addEventListener("click", () => {
-	if (mainPagePictureIndex > 0) {
-		mainPagePictureIndex--;
-	} else {
-		mainPagePictureIndex = mainPicBackgroundCollection.length - 1;
-	}
-	changePicture();
+  if (mainPagePictureIndex > 0) {
+    mainPagePictureIndex--;
+  } else {
+    mainPagePictureIndex = mainPicBackgroundCollection.length - 1;
+  }
+  changePicture();
 });
 
 //Next Picture
 changePicNext.addEventListener("click", () => {
-	if (mainPagePictureIndex < mainPicBackgroundCollection.length - 1) {
-		mainPagePictureIndex++;
-	} else {
-		mainPagePictureIndex = 0;
-	}
-	changePicture();
+  if (mainPagePictureIndex < mainPicBackgroundCollection.length - 1) {
+    mainPagePictureIndex++;
+  } else {
+    mainPagePictureIndex = 0;
+  }
+  changePicture();
 });
 
 function changePicture() {
-	mainPageBackground.src = mainPicBackgroundCollection[mainPagePictureIndex];
-	//Changes Slides When Changes Picture background
-	slideIndicators[mainPagePictureIndex].style.backgroundColor = "white";
-	for (let i = 0; i < mainPicBackgroundCollection.length; i++) {
-		if (i === mainPagePictureIndex) continue;
-		slideIndicators[i].style.backgroundColor = "";
-	}
+  mainPageBackground.src = mainPicBackgroundCollection[mainPagePictureIndex];
+  //Changes Slides When Changes Picture background
+  slideIndicators[mainPagePictureIndex].style.backgroundColor = "white";
+  for (let i = 0; i < mainPicBackgroundCollection.length; i++) {
+    if (i === mainPagePictureIndex) continue;
+    slideIndicators[i].style.backgroundColor = "";
+  }
 }
 
 //
-setInterval(function() {
-	if (mainPagePictureIndex < mainPicBackgroundCollection.length - 1) {
-		mainPagePictureIndex++;
-	} else {
-		mainPagePictureIndex = 0;
-	}
-	changePicture();
+setInterval(function () {
+  if (mainPagePictureIndex < mainPicBackgroundCollection.length - 1) {
+    mainPagePictureIndex++;
+  } else {
+    mainPagePictureIndex = 0;
+  }
+  changePicture();
 }, 12000);
 
 //Enable Search Bar
@@ -80,28 +80,32 @@ searchBar.classList.add("searchBarHidden");
 
 //Show Search Bar and Hide Header
 searchButton.addEventListener("click", () => {
-	header.classList.add("headerHidden");
-	searchBar.classList.add("searchBar");
-	searchBar.classList.remove("searchBarHidden");
+  header.classList.add("headerHidden");
+  searchBar.classList.add("searchBar");
+  searchBar.classList.remove("searchBarHidden");
 });
 
 //Close Search Bar and Show Header
 closeSearchBar.addEventListener("click", () => {
-	header.classList.remove("headerHidden");
-	searchBar.classList.remove("searchBar");
-	searchBar.classList.add("searchBarHidden");
+  header.classList.remove("headerHidden");
+  searchBar.classList.remove("searchBar");
+  searchBar.classList.add("searchBarHidden");
 });
 
 //Releases Cards
 let newReleases = document.querySelector("#newReleases");
-let releasingSoon = document.querySelector("#releasingSoon");
+let releasingSoons = document.querySelector("#releasingSoon");
 let newReleasesCollection = ["tite"];
-let releasingSoonCollection = [];
+let releasingSoonCollection = ["tite", "tite", "tite"];
 //Create Slide Indicators
 for (let i = 0; i < newReleasesCollection.length; i++) {
-	let newRelease = document.createElement("div");
-	newRelease.classList.add("ReleasesCard");
-	newReleases.appendChild(newRelease);
+  let newRelease = document.createElement("div");
+  newRelease.classList.add("ReleasesCard");
+  newReleases.appendChild(newRelease);
 }
 
-// for (let i = 0; i < releasingSoonCollection.length; i++) { }
+for (let i = 0; i < releasingSoonCollection.length; i++) {
+  let releasingSoon = document.createElement("div");
+  releasingSoon.classList.add("ReleasesCard");
+  releasingSoons.appendChild(releasingSoon);
+}
